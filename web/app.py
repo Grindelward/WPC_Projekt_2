08c49a46e2efa7d8042b4ee71eb7550307fc8b83
Grindelward <przemyslaw.nowicki93@gmail.com>
2018-05-12 09:09:01 +0000

    prorotype of form

--- web/app.py
+++ web/app.py
@@ -1,10 +1,16 @@
-from flaski import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Cypis solo"
+  return "You touch my tralala"
+
+@app.route("/order-animation")
+def order_animation():
+  return render_template(
+    "order.html",
+    invitation="Welocme")
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
